c99cee73d22a576c4c4582a0d25bdb1b08d9790e
NajlaAlmutari <najla_s_9@hotamil.com>
2017-05-27 17:04:31 -0300

    stage2 commit

--- app/src/main/java/com/acme/csci3130assig2/MainActivity.java
+++ app/src/main/java/com/acme/csci3130assig2/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.util.regex.Pattern;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -33,5 +34,33 @@ public class MainActivity extends AppCompatActivity {
 
             return MatchingRules;
         }
-}
-}
+
+        public static int Stage2ValidatingPassword(String password) {
+            int MatchingRules = 0;
+            // to get the 2 matching rules from stage1
+            MatchingRules += Stage1ValidatingPassword(password);
+
+            // test the password for both upper and lower case characters
+            if (Pattern.compile("[a-z]").matcher(password).find() && Pattern.compile("[A-Z]").matcher(password).find() ) {
+                System.out.println("Upper lower for " + password);
+                MatchingRules++;
+            }
+            // test the password if it has at-least one digit
+            if (Pattern.compile("[0-9]").matcher(password).find()) {
+                System.out.println("Digit for " + password);
+                MatchingRules++;
+            }
+            // test the password if it has at-least one special character   @,!,&, #,$
+            if (password.indexOf('@') != -1 || password.indexOf('!') != -1  || password.indexOf('&') != -1 || password.indexOf('#') != -1|| password.indexOf('$') != -1) {
+                MatchingRules++;
+            }
+            return MatchingRules;
+        }
+    }
+
+
+            }
+
+
+
+
---
+++ app/src/test/java/com/acme/csci3130assig2/ExampleUnitTest.java
@@ -0,0 +1,61 @@
+package com.acme.csci3130assig2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+
+
+    public class UserPasswordValidatorTest {
+
+        @Test
+        public void testStage1ValidatingPassword() {
+            // Rules:
+            // - it is not “password” (case insensitive)
+            // - it is at least 8 characters long
+
+            assertEquals(MainActivity.UserPasswordValidator.Stage1ValidatingPassword("PASSWOrD"), 1);
+            assertEquals(MainActivity.UserPasswordValidator.Stage1ValidatingPassword("XYZJ"), 1);
+            assertEquals(MainActivity.UserPasswordValidator.Stage1ValidatingPassword("XYZJKHIG"), 2);
+            assertEquals(MainActivity.UserPasswordValidator.Stage1ValidatingPassword("XYZJKHIGQR"), 2);
+        }
+}
+    @Test
+    public void testStage2ValidatingPassword() {
+        // Rules:
+        // 1 -  at least 8 characters long
+        // 2 -  not “password” (case insensitive)
+        // 3 - at least one upper and lower case character
+        // 4 - at least one special character @,!,&, #,$
+        // 5 - at least one digit
+
+        // rule 2, 3 matching
+        assertEquals(MainActivity.UserPasswordValidator.Stage2ValidatingPassword("Xyzj"), 2);
+
+        // rule 2, 3, 5 matching
+        assertEquals(MainActivity.UserPasswordValidator.Stage2ValidatingPassword("Xyzj57"), 3);
+
+        // rule 2, 3, 5, 1 matching
+        assertEquals(MainActivity.UserPasswordValidator.Stage2ValidatingPassword("Xyzj57kh"), 4);
+
+        // rule 2, 5, 1 matching
+        assertEquals(MainActivity.UserPasswordValidator.Stage2ValidatingPassword("xyzj57kh"), 3);
+
+        // rule 2, 3, 5, 1 matching
+        assertEquals(MainActivity.UserPasswordValidator.Stage2ValidatingPassword("Xyzj57kh"), 4);
+
+        // rule 2, 1, 4, 5, 3 matching
+        assertEquals(MainActivity.UserPasswordValidator.Stage2ValidatingPassword("Xyzj57$kh"), 5);
+    }
+}
